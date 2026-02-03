e06869bfe6c3fd68defdf7b405bf7deca6fd14d0
Joshua V. Dillon <jvdillon@gmail.com>
2026-02-02 19:29:33 -0800

    cleanup x182ll.py

--- code/sudoku/x182ll.py
+++ code/sudoku/x182ll.py
@@ -1,7 +1,7 @@
 """x182ll: x182j but seed=45"""
 
-from research.projects.trm3.experiment import main
-from research.projects.trm3.x182j import Experiment as Experiment182j
+from experiment import main
+from x182j import Experiment as Experiment182j
 
 
 class Experiment(Experiment182j):
@@ -9,4 +9,4 @@ class Experiment(Experiment182j):
 
 
 if __name__ == "__main__":
-    main(Experiment())  # pyright: ignore[reportArgumentType]
+    main(Experiment())
