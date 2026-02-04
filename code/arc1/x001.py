b94e8abe9a06e35ecd7edc4d2bd70e1c65ad94e6
Joshua V. Dillon <jvdillon@gmail.com>
2026-02-04 08:10:31 -0800

    setup arc training

---
+++ code/arc1/x001.py
@@ -0,0 +1,26 @@
+"""x001: Maze experiment wta, K_L=4."""
+
+from typing import cast
+
+import copy
+
+from arc1.x000 import Experiment as Experiment000
+from experiment import main
+from model import TRM3, TRM3ConfigProtocol
+
+
+def _config() -> TRM3.Config:
+    c = cast(TRM3.Config, copy.copy(Experiment000.config))
+    c.K_L = 4
+    c.carry_H = "copy_top1"
+    return c
+
+
+class Experiment(Experiment000):
+    batch_size: int = Experiment000.batch_size // 4
+    K: int = 4
+    config: TRM3ConfigProtocol = _config()
+
+
+if __name__ == "__main__":
+    main(Experiment())
