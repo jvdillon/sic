84810d1043361aaa0c846f2766d67835f5dbebef
Joshua V. Dillon <jvdillon@gmail.com>
2026-02-05 15:47:00 -0800

    add x000a

--- code/maze/x000a.py
+++ code/maze/x000a.py
@@ -1,17 +1,11 @@
 """x000a: x000 except larger lr."""
 
-from experiment import main, setup_muon_optimizers
+from experiment import main
 from maze.x000 import Experiment as Experiment000
 
 
 class Experiment(Experiment000):
     batch_size: int = 120
-
-    def setup_optimizers(self) -> None:
-        self.optimizer1, self.optimizer2 = setup_muon_optimizers(  # pyright: ignore[reportAttributeAccessIssue]
-            self.model,
-            muon_lr=0.005,
-        )
 
 
 if __name__ == "__main__":
